6460c74609ebcfe457d321f6bbddcab793e287b9
Casper <clydenberg@gmail.com>
2023-12-06 11:47:47 -0600

    SyntaxTypesVars

---
+++ src/SyntaxTypesVars.java
@@ -0,0 +1,62 @@
+public class SyntaxTypesVars {
+    public static void main(String[] args) {
+//        final String MY_NAME = "Casper";
+//        System.out.println(myName);
+//        char dollarSign = '$';
+//        System.out.println(dollarSign);
+//        int myAge = 25;
+//        long bigNumber = 1234567890;
+//        double decimal = 3.14;
+//        float PI = 3.14f;
+//
+//        int divide = (int) (bigNumber / myAge); //explicit casting
+//
+//        double divide2 = (bigNumber / myAge); //implicit casting
+
+        int myFavoriteNum = 8;
+        System.out.println(myFavoriteNum);
+
+        String myString = "Remember double quotes for strings";
+        System.out.println(myString);
+
+        float myNumber = 3.14f;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//
+//        int x2 = 6;
+//        System.out.println(++x2);
+//        System.out.println(x2);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        System.out.println(three);
+
+//        int three = (int) "three";
+//        System.out.println(three);
+
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+//
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+//        System.out.println(x);
+//        System.out.println(y);
+
+        int max = Integer.MAX_VALUE;
+        System.out.println(max);
+        max *= max;
+        System.out.println(max);
+    }
+}
